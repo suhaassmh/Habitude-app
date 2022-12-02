93cac05b82559014ea20706190736bcc8f278b8b
Suhaas MH <suhaas.smh@gmail.com>
2022-12-02 13:52:11 +0530

    initial commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:habitude_app/widgets/habit_tile.dart';
+import 'package:habitude_app/widgets/month_summary.dart';
+import 'package:habitude_app/widgets/my_fab.dart';
+import 'package:habitude_app/widgets/my_alert_box.dart';
+import 'package:habitude_app/data/habit_database.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Map<DateTime, int> heatMapDataSet = {};
+  HabitDatabase db = HabitDatabase();
+  final _myBox = Hive.box("Habit_Database");
+
+  @override
+  void initState() {
+    // if there is no current habit list, then it is the 1st time ever opening the app
+    // then create default data
+    if (_myBox.get("CURRENT_HABIT_LIST") == null) {
+      db.createDefaultData();
+    }
+    // else data already exsists? then load
+    else {
+      db.loadData();
+    }
+    db.updateDatabase();
+    super.initState();
+  }
+
+  void checkBoxTapped(bool? value, int index) {
+    setState(() {
+      db.todaysHabitList[index][1] = value;
+    });
+    db.updateDatabase();
+  }
+
+  // create a new habit
+  final _newHabitNameController = TextEditingController();
+  void createNewHabit() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          hintText: 'Enter habit name..',
+          onSave: saveNewHabit,
+          onCancel: cancelDialogBox,
+        );
+      },
+    );
+  }
+
+  // save new habit
+  void saveNewHabit() {
+    setState(() {
+      db.todaysHabitList.add([_newHabitNameController.text, false]);
+    });
+
+    // clear textfield
+    _newHabitNameController.clear();
+    // pop dialog box
+    Navigator.of(context).pop();
+    db.updateDatabase();
+  }
+
+  // cancel new habit
+  void cancelDialogBox() {
+    _newHabitNameController.clear();
+    Navigator.of(context).pop();
+  }
+
+  // open habit settings to edit
+  void openHabitSettings(int index) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return MyAlertBox(
+          controller: _newHabitNameController,
+          hintText: db.todaysHabitList[index][0],
+          onSave: () => saveExistingHabit(index),
+          onCancel: cancelDialogBox,
+        );
+      },
+    );
+  }
+
+  // save existing habit with a new name
+  void saveExistingHabit(int index) {
+    setState(() {
+      db.todaysHabitList[index][0] = _newHabitNameController.text;
+    });
+    _newHabitNameController.clear();
+    Navigator.pop(context);
+    db.updateDatabase();
+  }
+
+  // delete habit
+  void deleteHabit(int index) {
+    setState(() {
+      db.todaysHabitList.removeAt(index);
+    });
+    db.updateDatabase();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      floatingActionButton: MyFloatingActionButton(onPressed: createNewHabit),
+      body: ListView(
+        children: [
+          MonthlySummary(
+            datasets: db.heatMapDataSet,
+            startDate: _myBox.get("START_DATE"),
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: db.todaysHabitList.length,
+            itemBuilder: (context, index) {
+              return HabitTile(
+                habitName: db.todaysHabitList[index][0],
+                habitCompleted: db.todaysHabitList[index][1],
+                onChanged: (value) => checkBoxTapped(value, index),
+                settingsTapped: (context) => openHabitSettings(index),
+                deleteTapped: (context) => deleteHabit(index),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
